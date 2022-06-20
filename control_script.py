fbd2855c0e632b23b9c621a0d757d1df05355ba4
fun3sx <106584775+fun3sx@users.noreply.github.com>
2022-06-20 18:01:35 +0300

    added unemployment to control script

--- control_script.py
+++ control_script.py
@@ -9,12 +9,14 @@ import datetime as dt
 
 from update_gdp import main as main_gdp
 from update_hpi import main as main_hpi
+from update_unpl import main as main_unpl
 
 
 print (dt.datetime.now().strftime("%Y/%m/%d - %H:%M"))
 
 
 base_url = "https://api.interestingdata.eu/"
+#base_url = "127.0.0.1:5000"
 
 #check and update HPI
 
@@ -23,6 +25,11 @@ print (main_hpi(base_url+'hpi'))
 #check and update GDP
 
 print (main_gdp(base_url+'gdp'))
+
+#check and update unemployment
+
+print (main_unpl(base_url+'unpl'))
+
 
 
 
