202fbb8c53a47d971bdbd51907f451e390cb0dca
fun3sx <106584775+fun3sx@users.noreply.github.com>
2022-07-05 23:37:53 +0300

    control for enex

---
+++ run_enex.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  1 19:25:57 2022
+
+@author: User
+"""
+
+import datetime as dt
+
+from update_enex import main as main_enex
+
+print (dt.datetime.now().strftime("%Y/%m/%d - %H:%M"))
+
+
+base_url = "https://api.interestingdata.eu/"
+#base_url = "127.0.0.1:5000"
+
+#check and update enex data
+
+print (main_hpi(base_url+'enex'))
+
+
+
+
+print ('-'*10)
